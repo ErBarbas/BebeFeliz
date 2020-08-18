894b8b008689aec0bffd733ecfaf148b41e9316d
unknown <pinillacarlos892@gmail.com>
2020-08-18 14:45:01 -0500

    Se agrego un archivo de configuracion para Visual Code

---
+++ src/app/util/modules/material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+const myModules = [];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    myModules
+  ],
+  exports: [myModules]
+})
+export class MaterialModule { }

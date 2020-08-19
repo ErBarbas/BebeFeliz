0154258237afb0a8b478bfcf7e90f3c078bdde12
unknown <pinillacarlos892@gmail.com>
2020-08-18 21:46:47 -0500

    Se ha agregado el footer

--- src/app/util/modules/material.module.ts
+++ src/app/util/modules/material.module.ts
@@ -7,11 +7,12 @@ import { MatCardModule } from "@angular/material/card";
 import { MatDividerModule } from "@angular/material/divider";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
+import { MatListModule } from "@angular/material/list";
 
 //myModules id a variable that is reused in "imports" and "exports" to be able to use the modules of angular material in more than one module
 
 const myModules = [
-  MatToolbarModule, MatIconModule, MatButtonModule, MatCardModule, MatDividerModule, MatFormFieldModule, MatInputModule
+  MatToolbarModule, MatIconModule, MatButtonModule, MatCardModule, MatDividerModule, MatFormFieldModule, MatInputModule, MatListModule
 ]
 
 @NgModule({

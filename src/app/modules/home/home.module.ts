337d9e03e557b2e6d60bb5979c8d83b55dc58a2d
unknown <pinillacarlos892@gmail.com>
2020-08-18 20:34:06 -0500

    Se ha agregado la seccion de contacto

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import { HomeComponent } from './home.component';
   imports: [
     CommonModule,
     HomeRoutingModule,
+    ReactiveFormsModule,
     MaterialModule
   ]
 })
--- src/app/util/modules/material.module.ts
+++ src/app/util/modules/material.module.ts
@@ -4,11 +4,14 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 
 //myModules id a variable that is reused in "imports" and "exports" to be able to use the modules of angular material in more than one module
 
 const myModules = [
-  MatToolbarModule, MatIconModule, MatButtonModule, MatCardModule
+  MatToolbarModule, MatIconModule, MatButtonModule, MatCardModule, MatDividerModule, MatFormFieldModule, MatInputModule
 ]
 
 @NgModule({

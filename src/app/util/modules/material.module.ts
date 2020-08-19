39dd3dbd8fe498c34c364e8daf8bb83606a6196f
unknown <pinillacarlos892@gmail.com>
2020-08-18 19:11:08 -0500

    Se ha agregado la seccion de productos mas vendidos

--- src/app/util/modules/material.module.ts
+++ src/app/util/modules/material.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+
+//myModules id a variable that is reused in "imports" and "exports" to be able to use the modules of angular material in more than one module
 
 const myModules = [
-  MatToolbarModule, MatIconModule, MatButtonModule
+  MatToolbarModule, MatIconModule, MatButtonModule, MatCardModule
 ]
 
 @NgModule({

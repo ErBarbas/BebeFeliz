91e0c0a4800d16d6e9d080074f82efa1a49d90a9
unknown <pinillacarlos892@gmail.com>
2020-08-19 23:04:11 -0500

    Se ha agregado server side rendering

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     ToolbarComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     MaterialModule,
     BrowserAnimationsModule
